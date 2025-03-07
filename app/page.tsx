731e1097738058e2d7f6cd29d351a8278c7ad2b5
Bence T. <3326575+tbb98@users.noreply.github.com>
2025-03-07 21:57:13 +0100

    updated info

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+      <main className="flex flex-col gap-8 row-start-2 items-center">
+      <Image
           className="light:invert"
           src="/meshcore-logo.png"
           alt="MeshCore logo"
@@ -12,12 +12,33 @@ export default function Home() {
           height={48}
           priority
         />
-        <div className="list-inside list-decimal text-sm text-center sm:text-center font-[family-name:var(--font-geist-mono)]">
+        <div className="list-inside list-decimal text-center sm:text-center font-[family-name:var(--font-geist-mono)]">
             Üdvözlünk a magyar MeshCore közösség weboldalán.
         </div>
 
+        <div className="list-inside list-decimal text-sm text-center sm:text-center font-[family-name:var(--font-geist-mono)]">
+          Frekvencia: 869.525 MHz (a többi beállítás az alapértelmezett)
+          </div>
+
+        <a href="https://t.me/+63o5qlHPrl1iNjc0" target="_blank" rel="noopener noreferrer">
+          <button className="bg-gray-500 text-white font-bold py-4 px-4 rounded">
+            Telegram csoport
+          </button>
+        </a>
+
+        <a href="https://www.facebook.com/groups/421299027712536" target="_blank" rel="noopener noreferrer">
+          <button className="bg-gray-700 text-white font-bold py-4 px-4 rounded">
+            Facebook csoport
+          </button>
+        </a>
+
+        
+
        
       </main>
+
+
+
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 
       </footer>
