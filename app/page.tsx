5c9c95b56b22238ca1a1fdbbc1062e7e843ad3e9
Bence T. <3326575+tbb98@users.noreply.github.com>
2025-03-23 22:14:14 +0100

    updated info

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           className="light:invert"
           src="/meshcore-logo.png"
           alt="MeshCore logo"
-          width={350}
+          width={450}
           height={48}
           priority
         />
@@ -17,12 +17,12 @@ export default function Home() {
             Üdvözlünk a magyar MeshCore közösség weboldalán.
         </div>
 
-        <div className="list-inside list-decimal text-sm text-center sm:text-center font-[family-name:var(--font-geist-mono)]">
-          Frekvencia: 869.525 MHz (a többi beállítás az alapértelmezett)
+        <div className="list-inside max-w-3xl  list-decimal text-sm text-center sm:text-center font-[family-name:var(--font-geist-mono)]">
+          Frekvencia: 869.525 MHz - Spreading Factor SF11, Coding Rate 6, Bandwith: 250kHz (ezek nem az alapértelmezett beállítások, át kell állítani)
         </div>
 
 
-                <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-4">
           <a href="https://t.me/+63o5qlHPrl1iNjc0" target="_blank" rel="noopener noreferrer">
             <button className="bg-gray-500 text-white font-bold py-4 px-4 rounded w-full">
               Telegram csoport
@@ -58,22 +58,57 @@ export default function Home() {
           <h3 className="mt-4 font-bold">MeshCore üzemmódjai:</h3>
 
           <ul className="list-disc pl-5 space-y-4">
-          <li>
+           <li>
               <strong>Companion (Kliens)</strong> – Egy kliens módú eszköz, amely önmagában nem továbbítja az üzeneteket a hálózatba. Csak azokhoz az eszközökhöz tud üzenetet küldeni, amelyeket közvetlenül elér (például egy másik Companionhoz vagy egy Repeaterhez). Ez a szerepkör hasonló a Meshtastic <em>CLIENT_MUTE</em> módjához. Az útválasztásnak köszönhetően nagy (akár 64 hop) hálozatok is létrehozhatók.
             </li>
 
             <li>
-              <strong>Repeater (Bázisállomás)</strong> – A hálózat alapvető eleme, amely az üzeneteket fogadja és továbbítja. Legalább egy repeater szükséges a MeshCore hálózat működéséhez.
+              <strong>Repeater (Bázisállomás)</strong> – A hálózat alapvető eleme, amely az üzeneteket fogadja és továbbítja. Legalább egy repeater szükséges a MeshCore hálózat működéséhez. Csak a Repeaterek (és a Repeater módba kapcsolt Room Server) formálnak mesh hálózatot.
             </li>
 
             <li>
               <strong>Room Server</strong> – Egy opcionális működési mód, amely az alap feladatköre mellett repeaterként is működhet. Eltárolja az utolsó 16 üzenetet, és azokat elküldi minden újonnan csatlakozó Companion eszköznek.
             </li>
-          </ul>
-
+           </ul>
         </div>
 
+        <div className="text-justify max-w-3xl text-sm sm:text-base font-[family-name:var(--font-geist-mono)] space-y-4">
+          <h2 className="text-lg font-bold">Hogy tudom használni?</h2>
+          <p className="mt-2">
+          A legtöbb felhasználó egy mobilos appot használ ahhoz, hogy csevegjen a hálózaton. Léteznek önálló eszközök (pl. T-Deck), de ezek drágábbak, mint az úgynevezett Companion eszközök. Companionra azért van szükség, mert a mobiltelefonokban nincs LoRa-modul. Ezekhez Bluetooth-on lehet csatlakozni a telefonnal. Számítógép is használható; ilyenkor a Companion vagy Wi-Fi-n keresztül csatlakozik, vagy USB-vel a gépre.          </p>
+          
+          <h3 className="mt-4 font-bold">Szükséges lépések:</h3>
 
+          <ul className="list-disc pl-5 space-y-4">
+            <li>
+              <strong>MeshCore kompatibilis eszköz beszerzése</strong> (pl. Heltec v3, SeeedStudio T1000-E, RAK WisMesh), a támogatott eszközök a MeshCore hivatalos oldalán találhatók a Web Flasher menüpont alatt.
+            </li>
+
+            <li>
+              <strong>MeshCore telepítése</strong> a hivatalos oldalon található Web Flasher segítségével. (BT companion kell a telefonos használathoz)
+            </li>
+
+            <li>
+              <strong>A telefonos alkalmazás telepítése</strong>, ami elérhető a Google Play Store-ban és az Apple App Store-ban is.
+            </li>
+
+            <li>
+              <strong>A rádió paramétereinek beállítása</strong>, név, opcionálisan a hely, és a legfontosabb: a frekvencia, Spreading Factor és a Coding Rate. Ezek valószínűleg még változni fognak, ahogy kísérletezünk. A weboldalon mindig kint lesz a jelenlegi.
+            </li>
+
+            <li>
+              <strong>Advert</strong>: ahhoz, hogy mások megtudják, hogy a hálózaton vagy, küldeni kell egy Flood Router és egy Zero-Hop advertet is. Ez egy olyan üzenet, ami mindenkihez eljut (ha van olyan repeater, amit a rádiód elér) és jelzi, hogy a hálózaton vagy.
+            </li>
+
+            <li>
+              <strong>Várni</strong> kell, mert a legtöbb repeater 1-3 óránként küld csak advertet és addig nem fog senki látszódni.
+            </li>
+
+            <li>
+              <strong>Csatlakozni</strong> hozzánk Telegramon vagy Facebookon és zaklatni minket, hogy segítsünk.
+            </li>
+          </ul>
+        </div>
       </main>
 
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
